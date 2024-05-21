242c795ab05129174f3fb915d3e080f5b3f749e2
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-05-21 19:31:29 +0530

    index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ client.once('ready', () => {
   setTimeout(() => {
     console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
     client.user.setPresence({
-      activities: [{ name: `Type $help for Info`, type: ActivityType.Custom }],
+      activities: [{ name: `WZX STORE`, type: ActivityType.Custom }],
       status: 'dnd',
     });
   }, 2000); 
