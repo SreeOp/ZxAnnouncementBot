d2de6ebbe3fdf9ea84b49e6dd60b4948c0665c39
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-05-22 12:35:37 +0530

    gpay.js

--- commands/gpay.js
+++ commands/gpay.js
@@ -17,6 +17,7 @@ module.exports = {
         .setColor('#0099ff')
         .setTitle('Payment Method')
         .setDescription(`Google Pay`)
+        .setImage(`https://cdn.discordapp.com/attachments/1056903195961610275/1242682120141275176/standard_3.gif?ex=664eb969&is=664d67e9&hm=63d30f8533214bdab9e3660a6e4ee7a5a4f81db3cc3c796fe4b84d671c11c8be&`)
         .setTimestamp();
 
       sentMessage.edit({ embeds: [updatedPingEmbed] });
