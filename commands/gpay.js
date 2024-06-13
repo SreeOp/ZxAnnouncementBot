e944377adbdceebf9889d89d1a580b7cb34086f2
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-13 14:32:24 +0530

    gpay.js

--- commands/gpay.js
+++ commands/gpay.js
@@ -1,31 +1,16 @@
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: 'pay',
-  description: 'Send payment details with an image',
+  description: 'Display a list of available commands',
   execute(message, args, commandList) {
-    // Check if there are any arguments provided
-    if (args.length === 0) {
-      // Optionally, you can add a reply if desired, or remove this block entirely.
-      // message.reply('Please provide payment details.');
-      return;
-    }
-
-    // Join the arguments into a single string (assuming they represent payment details)
-    const paymentDetails = args.join(' ');
-
-    // Delete the user's message that triggered the command (if permissions allow)
-    message.delete().catch(console.error);
-
-    // Construct the embed message using MessageEmbed
-    const embed = new MessageEmbed()
+    const embed = new EmbedBuilder()
       .setColor('#0099ff')
-      .setTitle('Payment Details')
-      .setDescription(paymentDetails)
-      .setImage('https://cdn.discordapp.com/attachments/1167836302239084586/1198202736924168252/FN3.png')
-      // Replace with the actual URL of your image
+      .setTitle('Payment')
+      .setDescription('Pay')
+      .setImage('https://cdn.discordapp.com/attachments/1167836302239084586/1198202736924168252/FN3.png?ex=666b6982&is=666a1802&hm=1681987af9a97642fc1a9d136d010a3e293d349729eb122f10f2b56ae44d5561&')
 
-    // Send the embed message to the same channel
-    message.channel.send({ embeds: [embed] }).catch(console.error);
+
+   
   },
 };
