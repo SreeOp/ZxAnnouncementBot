cccf9617f4b4ba8b2629cc89fa97fa9863cb6874
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-13 13:44:11 +0530

    gpay.js

--- commands/gpay.js
+++ commands/gpay.js
@@ -1,33 +1,11 @@
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: 'pay',
-  description: 'Uploading Payment Scanner',
-  execute(message, args) {
-    // Delete the user's message that triggered the command (if permissions allow)
-    message.delete().catch(console.error);
-
-    const initialEmbed = new MessageEmbed()
+  description: 'Display a list of available commands',
+  execute(message, args, commandList) {
+    const embed = new EmbedBuilder()
       .setColor('#0099ff')
-      .setTitle('Gpay')
-      .setDescription('Uploading Payment Scanner')
-      .setTimestamp();
-
-    // Send the initial embed as a reply and store the sent message
-    message.reply({ embeds: [initialEmbed] }).then(sentMessage => {
-      // Calculate the time difference between message creation and reply creation
-      const ping = sentMessage.createdTimestamp - message.createdTimestamp;
-
-      // Create an updated embed with payment method details and an image
-      const updatedEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle('Payment Method')
-        .setDescription('Google Pay')
-        .setImage('https://cdn.discordapp.com/attachments/1056903195961610275/1242682120141275176/standard_3.gif')
-        .setTimestamp();
-
-      // Edit the sent message to update it with the new embed
-      sentMessage.edit({ embeds: [updatedEmbed] }).catch(console.error);
-    }).catch(console.error);
-  },
-};
+      .setTitle('Bot Commands')
+      .setDescription('▶️  **Here are the available commands :**\n‎ ')
+      .addImage('https://cdn.discordapp.com/attachments/1167836302239084586/1198202736924168252/FN3.png?ex=666b6982&is=666a1802&hm=1681987af9a97642fc1a9d136d010a3e293d349729eb122f10f2b56ae44d5561&')
