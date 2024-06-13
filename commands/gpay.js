ed95e6b2fdf576b41e28100b23769e26fcf2ed44
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-13 14:39:28 +0530

    gpay.js

--- commands/gpay.js
+++ commands/gpay.js
@@ -1,16 +1,17 @@
-const { EmbedBuilder } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   name: 'pay',
-  description: 'Display a list of available commands',
+  description: 'Display payment details with an image',
   execute(message, args, commandList) {
-    const embed = new EmbedBuilder()
+    // Create a new MessageEmbed object
+    const embed = new MessageEmbed()
       .setColor('#0099ff')
       .setTitle('Payment')
       .setDescription('Pay')
-      .setImage('https://cdn.discordapp.com/attachments/1167836302239084586/1198202736924168252/FN3.png?ex=666b6982&is=666a1802&hm=1681987af9a97642fc1a9d136d010a3e293d349729eb122f10f2b56ae44d5561&')
+      .setImage('https://cdn.discordapp.com/attachments/1167836302239084586/1198202736924168252/FN3.png');
 
-
-   
+    // Send the embed message to the same channel
+    message.channel.send({ embeds: [embed] }).catch(console.error);
   },
 };
