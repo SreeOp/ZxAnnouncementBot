c6c9d5924add4c397097a4b1004dbf3d8e307658
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-16 19:43:00 +0530

    index.js

--- index.js
+++ index.js
@@ -7,84 +7,86 @@ const { printWatermark } = require('./functions/handlers');
 const autoRoleHandler = require('./functions/autoRole'); // Import the autoRoleHandler function
 
 const client = new Client({
-  intents: Object.keys(GatewayIntentBits).map((a) => {
-    return GatewayIntentBits[a];
-  }),
+    intents: Object.keys(GatewayIntentBits).map((a) => {
+        return GatewayIntentBits[a];
+    }),
 });
 
 const prefix = '$';
-client.commands = new Map(); 
+client.commands = new Map();
 
+// Read all command files from the commands directory
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+// Dynamically load commands
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
 }
 
 client.on('messageCreate', (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const commandName = args.shift().toLowerCase();
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
 
-  const command = client.commands.get(commandName);
+    const command = client.commands.get(commandName);
 
-  if (!command) return;
+    if (!command) return;
 
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply('There was an error trying to execute that command!');
-  }
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('There was an error trying to execute that command!');
+    }
 });
 
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  const imagePath = path.join(__dirname, 'index.html');
-  res.sendFile(imagePath);
+    const indexPath = path.join(__dirname, 'index.html');
+    res.sendFile(indexPath);
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
+    console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
 });
 printWatermark();
 
 async function login() {
-  try {
-    await client.login(process.env.TOKEN);
-    console.log('\x1b[32m%s\x1b[0m', '|    🍔 Bot logged in successfully!');
-    console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded successfully!');
-    console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
-    console.log('\x1b[36m%s\x1b[0m', `|    🏡 Bot is in ${client.guilds.cache.size} servers`);
-  } catch (error) {
-    console.error('\x1b[31m%s\x1b[0m', '❌ Failed to log in:', error);
-    console.log('\x1b[31m%s\x1b[0m', '❌ Client Not Login, Restarting Process...');
-    process.kill(1);
-  }
+    try {
+        await client.login(process.env.TOKEN);
+        console.log('\x1b[32m%s\x1b[0m', '|    🍔 Bot logged in successfully!');
+        console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded successfully!');
+        console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
+        console.log('\x1b[36m%s\x1b[0m', `|    🏡 Bot is in ${client.guilds.cache.size} servers`);
+    } catch (error) {
+        console.error('\x1b[31m%s\x1b[0m', '❌ Failed to log in:', error);
+        console.log('\x1b[31m%s\x1b[0m', '❌ Client Not Login, Restarting Process...');
+        process.kill(1);
+    }
 }
 
 client.once('ready', () => {
-  setTimeout(() => {
-    console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity successfully set!`);
-    client.user.setPresence({
-      activities: [{ name: `WZX STORE`, type: ActivityType.Custom }],
-      status: 'dnd',
-    });
-  }, 2000);
+    setTimeout(() => {
+        console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity successfully set!`);
+        client.user.setPresence({
+            activities: [{ name: `WZX STORE`, type: ActivityType.Custom }],
+            status: 'dnd',
+        });
+    }, 2000);
 
-  const autoRoleId = '1251558263632167052'; // Replace with your auto role ID
-  autoRoleHandler(client, autoRoleId); // Call the auto-role handler function
+    const autoRoleId = '1251558263632167052'; // Replace with your auto role ID
+    autoRoleHandler(client, autoRoleId); // Call the auto-role handler function
 });
 
 login();
 
 setInterval(() => {
-  if (!client || !client.user) {
-    console.log('\x1b[31m%s\x1b[0m', '❌ Client Not Logged in, Restarting Process...');
-    process.kill(1);
-  }
+    if (!client || !client.user) {
+        console.log('\x1b[31m%s\x1b[0m', '❌ Client Not Logged in, Restarting Process...');
+        process.kill(1);
+    }
 }, 15000);
 
 module.exports = client;
