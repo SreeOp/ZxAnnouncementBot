98580bcce02fb8dcad8bde36cb731e49f3b37ff4
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-17 21:28:57 +0530

    store.js

--- commands/store.js
+++ commands/store.js
@@ -1,4 +1,8 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const storeDataPath = path.resolve(__dirname, 'storeData.json');
 
 module.exports = {
     name: 'store',
@@ -10,17 +14,41 @@ module.exports = {
 
         const [imageUrl, downloadLabel, downloadLink, videoLink] = args;
 
+        // Load existing store data
+        let storeData;
+        try {
+            storeData = JSON.parse(fs.readFileSync(storeDataPath, 'utf8'));
+        } catch (error) {
+            console.error('Error reading store data:', error);
+            storeData = {};
+        }
+
+        // Generate a unique ID for this store item
+        const storeItemId = `item-${Date.now()}`;
+
+        // Save the download link in the "database" (storeData.json)
+        storeData[storeItemId] = {
+            downloadLabel,
+            downloadLink
+        };
+
+        try {
+            fs.writeFileSync(storeDataPath, JSON.stringify(storeData, null, 2), 'utf8');
+        } catch (error) {
+            console.error('Error writing store data:', error);
+            return message.channel.send('There was an error while trying to save the store data.');
+        }
+
         const embed = new EmbedBuilder()
             .setColor('#BC13FE')
             .setTitle('ZX STORE')
             .setDescription('Click the buttons below to download or watch the video.')
-            .setImage(imageUrl)
-            .setFooter({ text: downloadLink }); // Store the download link in the footer for later retrieval
+            .setImage(imageUrl);
 
         const row = new ActionRowBuilder()
             .addComponents(
                 new ButtonBuilder()
-                    .setCustomId('download')
+                    .setCustomId(`download-${storeItemId}`) // Use the store item ID as part of the custom ID
                     .setLabel(downloadLabel)
                     .setStyle(ButtonStyle.Primary),
                 new ButtonBuilder()
