418947c258302f527d9d2c336b5c88ad947ef27b
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-17 16:54:13 +0530

    database.js

--- data/database.js
+++ data/database.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-const databasePath = path.join(__dirname, 'data', 'database.json');
+// Ensure the data directory exists
+const dataDir = path.join(__dirname);
+if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir);
+}
 
+// Path to the database file
+const databasePath = path.join(dataDir, 'database.json');
+
+// Function to read the database
 function readDatabase() {
     if (!fs.existsSync(databasePath)) {
         fs.writeFileSync(databasePath, JSON.stringify({}));
@@ -11,16 +19,19 @@ function readDatabase() {
     return JSON.parse(data);
 }
 
+// Function to write to the database
 function writeDatabase(data) {
     fs.writeFileSync(databasePath, JSON.stringify(data, null, 2));
 }
 
+// Function to add a download link
 function addDownloadLink(messageId, downloadLink) {
     const db = readDatabase();
     db[messageId] = downloadLink;
     writeDatabase(db);
 }
 
+// Function to get a download link
 function getDownloadLink(messageId) {
     const db = readDatabase();
     return db[messageId];
