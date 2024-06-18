10634202c23e8c5ae3ca92467acc994ae38b5c7d
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-18 17:43:22 +0530

    Update welcomeMessage.js

--- functions/welcomeMessage.js
+++ functions/welcomeMessage.js
@@ -1,16 +1,16 @@
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = (client) => {
     client.on('guildMemberAdd', async (member) => {
         try {
             // Create an embed message
-            const welcomeEmbed = new MessageEmbed()
+            const welcomeEmbed = new EmbedBuilder()
                 .setColor('#0099ff')
                 .setTitle('Welcome to the Server!')
                 .setDescription(`Hello ${member.user.username}, welcome to ${member.guild.name}! We're glad to have you here.`)
                 .setThumbnail(member.user.avatarURL())
                 .setTimestamp()
-                .setFooter('Enjoy your stay!', member.guild.iconURL());
+                .setFooter({ text: 'Enjoy your stay!', iconURL: member.guild.iconURL() });
 
             // Send the embed to the user's DMs
             await member.send({ embeds: [welcomeEmbed] });
