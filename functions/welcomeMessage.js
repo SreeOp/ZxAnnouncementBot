ba126f8a09e67013173fc5e6a701d59d889e708b
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-18 17:50:33 +0530

    welcomeMessage.js

--- functions/welcomeMessage.js
+++ functions/welcomeMessage.js
@@ -1,19 +1,24 @@
 const { EmbedBuilder } = require('discord.js');
 
-module.exports = (client) => {
+module.exports = (client, welcomeChannelId) => {
     client.on('guildMemberAdd', async (member) => {
         try {
             // Create an embed message
             const welcomeEmbed = new EmbedBuilder()
-                .setColor('#0099ff')
+                .setColor('#BC13FE')
                 .setTitle('Welcome to the Server!')
                 .setDescription(`Hello ${member.user.username}, welcome to ${member.guild.name}! We're glad to have you here.`)
                 .setThumbnail(member.user.avatarURL())
                 .setTimestamp()
                 .setFooter({ text: 'Enjoy your stay!', iconURL: member.guild.iconURL() });
 
-            // Send the embed to the user's DMs
-            await member.send({ embeds: [welcomeEmbed] });
+            // Send the embed to the specified channel
+            const channel = member.guild.channels.cache.get(welcomeChannelId);
+            if (channel) {
+                await channel.send({ embeds: [welcomeEmbed] });
+            } else {
+                console.error('Welcome channel not found.');
+            }
         } catch (error) {
             console.error('Error sending welcome message:', error);
         }
