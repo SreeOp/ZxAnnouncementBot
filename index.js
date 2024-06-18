3cbc618078659f8f916eef0a9c333bba1251d300
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-18 20:46:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ client.once('ready', () => {
     }, 2000);
 
     const autoRoleId = '1251558263632167052'; // Replace with your auto role ID
-    const welcomeChannelId = '1249329942328512614'; // Replace with your welcome channel ID
+    const welcomeChannelId = 'YOUR_WELCOME_CHANNEL_ID'; // Replace with your welcome channel ID
 
     autoRoleHandler(client, autoRoleId); // Call the auto-role handler function
     welcomeMessageHandler(client, welcomeChannelId); // Call the welcome message handler function
