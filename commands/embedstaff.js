f259c5a61e71418c1ee3c481728008d02c6bf16b
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-06-28 20:28:16 +0530

    embedstaff.js

--- commands/embedstaff.js
+++ commands/embedstaff.js
@@ -1,8 +1,8 @@
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
     name: 'embedstaff',
-    description: 'Send an embedded message with an image and a Show Staff button.',
+    description: 'Send an embedded message with an image and a show staff button.',
     async execute(message, args) {
         if (args.length < 1) {
             return message.channel.send('Usage: $embedstaff [image_url]');
@@ -10,23 +10,22 @@ module.exports = {
 
         const [imageUrl] = args;
 
-        const embed = new MessageEmbed()
+        const embed = new EmbedBuilder()
             .setColor('#BC13FE')
             .setTitle('Staff Information')
-            .setDescription('Click the button below to view the staff members.')
+            .setDescription('Click the button below to show staff members.')
             .setImage(imageUrl);
 
-        const row = new MessageActionRow()
+        const row = new ActionRowBuilder()
             .addComponents(
-                new MessageButton()
+                new ButtonBuilder()
                     .setCustomId('show_staff')
                     .setLabel('Show Staff')
-                    .setStyle('PRIMARY')
+                    .setStyle(ButtonStyle.Primary)
             );
 
         try {
             await message.channel.send({ embeds: [embed], components: [row] });
-            await message.delete();
         } catch (error) {
             console.error('Error sending message:', error);
             message.channel.send('There was an error while trying to send the embed message.');
